df30247c6d0bc7da77ce3e31b937f6cb506af6f5
SeptianFaris04 <farisasmita@gmail.com>
2024-11-22 19:07:22 +0700

    Update

--- resources/views/Components/nav-link.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" 
-aria-current="page 
--->
-
-
-<a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
